d32b00b0845b3bd9de7014510b4c0f24eb5f929c
Semih Baltacıoğlu <semihsame@hotmail.com>
2021-07-01 00:35:49 +0300

    basit layout başlangıç

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-drawer
+      show-if-above
+      side="left"
+      bordered
+      width="500"
+    >
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+export default {
+  name: 'MainLayout',
+
+
+}
+
+</script>
